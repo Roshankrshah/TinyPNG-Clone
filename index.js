c596a4ee7fc769e3147aad19ee3a8f2d9706f6b0
Roshan <shahroshan0720@gmail.com>
2023-09-28 10:35:27 +0530

    setup public folder

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import path from "path";
+import fileDirName from "./utils/dirname.js";
 
 const app = express();
+
+const {__dirname } = fileDirName(import.meta);
+const publicDirPath = path.join(__dirname,'public');
+
+app.use(express.static(publicDirPath));
 
 app.use(morgan('dev'));
 
 app.get('/',(req,res)=>{
-    res.send('Stating TinyPNG project')
+    const homeDir = path.join(__dirname,'public','home.html');
+    res.sendFile(homeDir);
 });
 
 const port = 2611 || process.env.PORT;
