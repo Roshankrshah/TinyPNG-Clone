3a6ca77675f576720b0ecae5008fa8fa78b9afff
Roshan <shahroshan0720@gmail.com>
2023-09-28 12:49:35 +0530

    download route setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,35 @@
 import path from 'path';
 import fileDirName from '../utils/dirname.js';
 
-const {__dirname } = fileDirName(import.meta);
+const { __dirname } = fileDirName(import.meta);
 
-export async function getHomePage(req,res){
-    const homeDir = path.join(__dirname,'..','public','home.html');
-    res.sendFile(homeDir);
+export async function getHomePage(req, res) {
+    try {
+        const homeDir = path.join(__dirname, '..', 'public', 'home.html');
+        res.sendFile(homeDir);
+    } catch (e) {
+        res.status(500).json({ error: "Server error" });
+        console.log(e);
+    }
 }
 
-export async function postResizeImages(req,res){
-    console.log(req.files);
-    res.json({foo: "bar"});
+export async function postResizeImages(req, res) {
+    try {
+        console.log(req.files);
+        res.json({ downloadLinks: ['/image_1.png'] });
+    } catch (e) {
+        res.status(500).json({ error: "Server error" });
+        console.log(e);
+    }
+}
+
+
+export async function getDownloadFileById(req,res){
+    try {
+        //res.download()
+        res.json({foo: "baar"});
+    } catch (e) {
+        res.status(500).json({ error: "Server error" });
+        console.log(e);
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { getHomePage, postResizeImages } from '../controllers/index.js';
+import { getHomePage, postResizeImages,getDownloadFileById } from '../controllers/index.js';
 import {uploadsMiddleware} from '../middlewares/uploads.js';
 
 const router = Router();
 
 router.get('/',getHomePage);
-router.post('/resize-images',uploadsMiddleware,postResizeImages)
+router.post('/resize-images',uploadsMiddleware,postResizeImages);
+router.get('/download/:fileId',getDownloadFileById);
 
 export default router;
